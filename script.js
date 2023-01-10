a1df8a8de7660440335b5b089e5c3d0aaaf5f3de
Milla2000 <millajesso2000@gmail.com>
2023-01-10 12:03:06 +0300

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 
+const playerScoreEl = document.getElementById('playerScore');
+const playerChoiceEl = document.getElementById('playerChoice');
+const playerChoiceall= document.getElementById('player');
+const computerScoreEl = document.getElementById('computerScore');
+const computerChoiceEl = document.getElementById('computerChoice');
+
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper');
+const playerScissors = document.getElementById('playerScissors');
+const playerLizard = document.getElementById('playerLizard');
+const playerSpock = document.getElementById('playerSpock');
+
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissors = document.getElementById('computerScissors');
+const computerLizard = document.getElementById('computerLizard');
+const computerSpock = document.getElementById('computerSpock');
+
+//const allGameIcons = document.querySelectorAll('.far');
+const resultText = document.getElementById('resultText');
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
@@ -8,3 +28,47 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+// passing player selection value and styling icons
+
+// var myLink = document.getElementById('player');
+// myLink.onclick = function (){
+//      console.log()
+// }
+var icon = document.querySelectorAll("i")
+
+
+function myselect (event) {
+  var clickedicon = event.target;
+   switch(clickedicon.id) {
+    case "playerRock" :
+      console.log('hello')
+      playerRock.classList.add('selected');
+      playerChoiceEl.textContent = '---Rock';
+      break;
+
+    case 'playerPaper':
+      playerPaper.classList.add('selected');
+      playerChoiceEl.textContent = '---Paper';
+      break;
+
+    case 'playerScissors':
+      playerScissors.classList.add('selected');
+      playerChoiceEl.textContent = '---Scissors';
+      break;
+
+    case 'playerLizard':
+      playerLizard.classList.add('selected');
+      playerChoiceEl.textContent = '---Lizard';
+      break;
+      
+    case 'playerSpock':
+      playerSpock.classList.add('selected');
+      playerChoiceEl.textContent = '---Spock';
+      break;
+    default:
+      break;
+
+  }  
+    };
+
+playerChoiceall.addEventListener("click", myselect);
